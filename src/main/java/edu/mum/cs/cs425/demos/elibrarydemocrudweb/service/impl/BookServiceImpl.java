ca28a04b5db01bce91e322ffea35a1a26a10381d
Soroncho <msoroncho@gmail.com>
2019-08-03 16:03:42 -0500

    First commit

---
+++ src/main/java/edu/mum/cs/cs425/demos/elibrarydemocrudweb/service/impl/BookServiceImpl.java
@@ -0,0 +1,55 @@
+package edu.mum.cs.cs425.demos.elibrarydemocrudweb.service.impl;
+
+import edu.mum.cs.cs425.demos.elibrarydemocrudweb.model.Book;
+import edu.mum.cs.cs425.demos.elibrarydemocrudweb.repository.BookRepository;
+import edu.mum.cs.cs425.demos.elibrarydemocrudweb.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Override
+    public Iterable<Book> getAllBooks() {
+        return repository.findAll(Sort.by("title"));
+    }
+
+    @Override
+    public Page<Book> getAllBooksPaged(int pageNo) {
+        return repository.findAll(PageRequest.of(pageNo, 3, Sort.by("title")));
+    }
+
+    @Override
+    public Book saveBook(Book book) {
+        return repository.save(book);
+    }
+
+    @Override
+    public Book getBookById(Integer bookId) {
+        return repository.findById(bookId).orElse(null);
+    }
+
+    @Override
+    public void deleteBookById(Integer bookId) {
+        repository.deleteById(bookId);
+    }
+
+    @Override
+    public Page<Book> search(String search,int pageNo) {
+        Page<Book> students  =repository.findBooksByTitleContainsOrIsbnContainsOrPublisherContains(search,search,search, PageRequest.of(pageNo, 3, Sort.by("title")));
+        return students;
+    }
+
+}
